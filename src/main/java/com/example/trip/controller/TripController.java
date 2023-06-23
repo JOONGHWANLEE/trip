d9358337ae7eb175766c541a58a3929a312a070e
JOONGHWANLEE <pppy11@naver.com>
2023-06-23 09:29:30 +0900

    06.23 up

--- src/main/java/com/example/trip/controller/TripController.java
+++ src/main/java/com/example/trip/controller/TripController.java
@@ -27,7 +27,7 @@ public class TripController {
 	
 	@RequestMapping(value = "/")
 	public String index() {
-		return "/index";
+		return "index";
 	}
 	
 	//get방식으로 request가 들어올때
@@ -87,7 +87,7 @@ public class TripController {
 				int dataCount = tripService.getDataCount(searchKey, searchValue);
 				
 				//2. 페이징 처리를 한다(준비 단계)
-				int numPerPage = 5; //페이지당 보여줄 데이터의 갯수
+				int numPerPage = 2; //페이지당 보여줄 데이터의 갯수
 				int totalPage = myUtil.getPageCount(numPerPage, dataCount); //페이지의 전체 갯수를 구한다
 				
 				if(currentPage > totalPage) currentPage = totalPage; //totalPage보다 크면 안된다
